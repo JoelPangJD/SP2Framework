2d3bbc3352403869b1afef4323f52002f0b02c9f
Mirai-UwU <69341830+Mirai-UwU@users.noreply.github.com>
2021-02-15 13:43:13 +0800

    Added Inventory UI
    Added inventory UI and move FPS to top right corner

--- StudioProject2/Application/Inventory.h
+++ StudioProject2/Application/Inventory.h
@@ -1,1 +1,20 @@
 #pragma once
+
+class Inventory
+{
+public:
+	Inventory();
+	~Inventory();
+	int position(int _x, int _y)
+	{
+		x = _x;
+		y = _y;
+	}
+
+
+
+private:
+	int x = 0;
+	int y = 0;
+
+};
