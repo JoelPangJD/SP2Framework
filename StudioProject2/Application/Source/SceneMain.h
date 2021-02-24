727996c7e4a802d87e8cc9a1f0b00fc24febdc3b
Fou <therealjo3d3su@gmail.com>
2021-02-24 16:00:22 +0800

    converted the render functions

--- StudioProject2/Application/Source/SceneMain.h
+++ StudioProject2/Application/Source/SceneMain.h
@@ -120,10 +120,6 @@ private:
 
 	void RenderSkybox();
 	void RenderUI();
-	void RenderMesh(Mesh* mesh, bool enableLight);
-	void RenderText(Mesh* mesh, std::string text, Color color);
-	void RenderTextOnScreen(Mesh* mesh, std::string text, Color color, float size, float x, float y);
-	void RenderMeshOnScreen(Mesh* mesh, float x, float y, int sizex, int sizey);
 
 	void RenderMinigame();
 	void RenderNPCDialogue(std::string NPCText, std::string headerText);
