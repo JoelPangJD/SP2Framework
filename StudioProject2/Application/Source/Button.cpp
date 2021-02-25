21aa3fd5d3720f2d98a4a81b2c8fdc59e7b40694
Mirai-UwU <69341830+Mirai-UwU@users.noreply.github.com>
2021-02-25 11:49:03 +0800

    update sceneMuseum
    uses button function for minigame1 and exit museum

--- StudioProject2/Application/Source/Button.cpp
+++ StudioProject2/Application/Source/Button.cpp
@@ -6,6 +6,7 @@ Button::Button()
 	positionX = positionY = width = height = 0;
 	clickedOn = active = false;
 	bLButtonState = true;
+	hold = false;
 }
 
 Button::Button(float positionX, float positionY, float width, float height, bool isActive)
@@ -41,6 +42,7 @@ void Button::updateButton()
 			float posY = 60 - y / 10;
 			if (posX > positionX && posX < (positionX + width) && posY > positionY && posY < (positionY + height)) {
 				clickedOn = true;
+				hold = true;
 			}
 			bLButtonState = false;
 		}
@@ -51,6 +53,7 @@ void Button::updateButton()
 		{
 			clickedOn = false;
 			bLButtonState = true;
+			hold = false;
 		}
 	}
 }
